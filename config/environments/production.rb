1baeef1a6f010381f392103d80f006a8c9a708bf
makedon <engineer.plusplus@gmail.com>
2025-06-17 22:01:24 -0600

    removed the hostnames from pro.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,9 +2,9 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
 
-  config.action_controller.asset_host = "https://spree-web-d5pu.onrender.com"
-  config.action_mailer.default_url_options = "https://spree-web-d5pu.onrender.com"
-  config.action_controller.default_url_options = "https://spree-web-d5pu.onrender.com"
+  # config.action_controller.asset_host = "https://spree-web-d5pu.onrender.com"
+  # config.action_mailer.default_url_options = "https://spree-web-d5pu.onrender.com"
+  # config.action_controller.default_url_options = "https://spree-web-d5pu.onrender.com"
 
   # Settings specified here will take precedence over those in config/application.rb.
 
